c27457692f641cc35b8df43926cf1b0ce744116f
Chapman-John <52362698+Chapman-John@users.noreply.github.com>
2024-09-01 19:10:15 -0700

    src and test added

---
+++ src/main/java/com/bank/banking_online/BankingOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.bank.banking_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/banking_online/BankingOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.banking_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
